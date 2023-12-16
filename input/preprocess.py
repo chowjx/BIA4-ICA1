52b2157ff8affb5ed266feecccc604694e35a0e6
lujingrui01 <108412353+lujingrui01@users.noreply.github.com>
2023-12-16 18:07:40 +0800

    preprocess.py

--- input/preprocess.py
+++ input/preprocess.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import os
 from skimage.io import imread, imsave
-import matplotlib.pyplot as plt
 import cv2
 from skimage.exposure import equalize_hist
 from skimage import img_as_ubyte
@@ -10,51 +9,49 @@ from skimage.filters import median, gaussian
 
 
 
-normal = pd.read_excel('../input/tb-chest-radiography/TB_Chest_Radiography_Database/Normal.metadata.xlsx')
-tb = pd.read_excel('../input/tb-chest-radiography/TB_Chest_Radiography_Database/Tuberculosis.metadata.xlsx')
-count_normal=len(normal)
-count_tb=len(tb)
-print("There are "+ str(count_normal)+" images of Normal and "+str(count_tb)+" images of Tuberculosis.")
-print("......Image preprocessing......")
-
 def convert_to_RGB_image(image):
-    #Save all the grayscale images as RGB
+    #Convert all the grayscale images as RGB, since some images are not identical in all three RGB channels
     if len(image.shape) == 2:
         image = np.stack((image,) * 3, axis=-1)
     else:
         image
     return image
 
-def cut_white_edge(image,threshold1=254,threshold2=255):
-    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray_img, threshold1, threshold2, cv2.THRESH_BINARY_INV)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def cut_white_edge(image,thres1=250,thres2=255):
+    # Convert to grayscale image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    a, thres = cv2.threshold(gray, thres1, thres2, cv2.THRESH_BINARY_INV)
+    # Find the contour of this image
+    contour, b = cv2.findContours(thres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Cut the white edge
     max_area=0
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > max_area:
-            max_area = area
-            x, y, w, h = cv2.boundingRect(cnt)
-    return image[y:y+h, x:x+w]
+    for c in contour:
+        if cv2.contourArea(c) > max_area:
+            max_area = cv2.contourArea(c)
+            m, n, p, q = cv2.boundingRect(c)
+    return image[n:n+q, m:m+p]
         
 def resize_and_rescale(image, size=(320, 320)):
-    image_resized = cv2.resize(image, size) # Resize image
-    image_rescaled = image_resized / 255.0    # Rescale pixel values
+    # Resize the image
+    image_resized = cv2.resize(image, size)
+    # Rescale pixel values
+    image_rescaled = image_resized / 255.0
     return image_rescaled
 
 def enhance_contrast(image):
     # Enhance contrast using histogram equalization
-    (b, g, r) = cv2.split(image)
+    # Perform this step in both b, g, r channels
+    b, g, r = cv2.split(image)
     be = cv2.equalizeHist(b)
     ge = cv2.equalizeHist(g)
     re = cv2.equalizeHist(r)
-    equalised_image = cv2.merge((be, ge, re))
-    return equalised_image
+    equalized_image = cv2.merge((be, ge, re))
+    return equalized_image
 
 def reduce_noise(image):
     # Perform noise reduction through Gaussian blur
-    filter_image = gaussian(image, sigma=1,channel_axis=-1)
-    return filter_image
+    denoised_image = gaussian(image, sigma=1,channel_axis=-1)
+    return denoised_image
 
 def preprocess_image(image_path, size=(320, 320)):
     # read the images
@@ -67,18 +64,28 @@ def preprocess_image(image_path, size=(320, 320)):
     image = reduce_noise(image)
     return image
 
+            
+def process_input(input_path, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # Deal with single image file
+    if os.path.isfile(input_path) and input_path.lower().endswith(('.png', '.jpg', '.jpeg')):
+        processed_image = preprocess_image(input_path, size=(320, 320))
+        fn = os.path.basename(input_path)
+        imsave(os.path.join(output_folder, fn), img_as_ubyte(processed_image))
+    # Deal with a folder
+    elif os.path.isdir(input_path):
+        for fn in os.listdir(input_path):
+            if fn.lower().endswith(('.png', '.jpg', '.jpeg')):
+                file_path = os.path.join(input_path, fn)
+                processed_image = preprocess_image(file_path, size=(320, 320))
+                imsave(os.path.join(output_folder, fn), img_as_ubyte(processed_image))
+    else:
+        print("The provided path is neither a file nor a directory. Please check your input path.")
 
-def process_folder(input_folder, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    for filename in os.listdir(input_folder):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            file_path = os.path.join(input_folder, filename)
-            processed_image = preprocess_image(file_path,size=(320, 320))
-            imsave(os.path.join(output_folder, filename), img_as_ubyte(processed_image))
 
-process_folder('../input/tb-chest-radiography/TB_Chest_Radiography_Database/Normal', 'processed_normal')
-process_folder('../input/tb-chest-radiography/TB_Chest_Radiography_Database/Tuberculosis', 'processed_tb')
+process_input('../input/tb-chest-radiography/TB_Chest_Radiography_Database/Normal', 'processed_normal')
+process_input('../input/tb-chest-radiography/TB_Chest_Radiography_Database/Tuberculosis', 'processed_tb')
 print("Data preprocessing completed")
 
 
